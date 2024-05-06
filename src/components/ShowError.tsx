28e68530ff9a651785f57307db4295a989f172a3
iampiku <iampikuchatterjee@gmail.com>
2024-05-06 10:23:03 +0530

    Error component added

--- src/components/ShowError.tsx
+++ src/components/ShowError.tsx
@@ -1,20 +1,41 @@
-import { useMemo } from "react";
+import { Image } from "@nextui-org/react";
+import { memo } from "react";
 
 interface Props {
 	errorCode: 404 | 400 | 500;
 }
 
-export default function ShowError({ errorCode }: Readonly<Props>) {
-	const errorMessage: { title: string; subtitle: string } = useMemo(() => {
-		const errorMessageMapping: {
-			[key: number]: { title: string; subtitle: string };
-		} = {
-			404: {
-				title: "Item not found!",
-				subtitle: "The item you were looking for does'nt exits",
-			},
-		};
-		return errorMessageMapping[errorCode];
-	}, [errorCode]);
-	return <span>Oops! something went wrong {errorCode}</span>;
-}
+const ShowError = memo(({ errorCode }: Readonly<Props>) => {
+	const errorMessage: {
+		[key: number]: { title: string; subtitle: string };
+	} = {
+		404: {
+			title: "Item not found!",
+			subtitle: "The item you were looking for does'nt exits",
+		},
+		400: {
+			title: "Bad Request!",
+			subtitle: "The request you made is invalid",
+		},
+		500: {
+			title: "Internal Server Error!",
+			subtitle: "Something went wrong on the server",
+		},
+	} as const;
+
+	const errorIcon = {
+		404: "",
+		400: "",
+		500: "",
+	} as const;
+
+	return (
+		<div className="">
+			<Image src={errorIcon[errorCode]}></Image>
+			<strong>{errorMessage[errorCode].title}</strong>
+			<p>{errorMessage[errorCode].subtitle}</p>
+		</div>
+	);
+});
+
+export default ShowError;
